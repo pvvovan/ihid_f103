95b7d3a67f274cba01f5d151e8609dae0a7cb327
PVVovan <pvvovan@gmail.com>
2026-07-30 11:22:15 +0200

    Move variable declartion to header file

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -44,7 +44,7 @@
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
-extern USBD_HandleTypeDef hUsbDeviceFS;
+
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 cmake -S . -B linbuild -D CMAKE_TOOLCHAIN_FILE=cmake/gcc-arm-none-eabi.cmake
-cmake --build ./linbuild
+cmake --build linbuild
