7e5d1cca4e0223fe8b2346f78611965586d86314
PVVovan <pvvovan@gmail.com>
2026-08-02 11:06:35 +0200

    Separate --build from --preset

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 cd ${SCRIPT_DIR}
-cmake -S . --preset Debug
-cmake --build --preset Debug
+cmake -S . --preset=Debug
+cmake --build --preset=Debug
