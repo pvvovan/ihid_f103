3bfb6db505f3ff432ecc32b1910723515a7f714a
Volodymyr Petrovskyy <pvvovan@gmail.com>
2026-08-09 11:05:28 +0200

    Run USB stack from OS task

--- Core/Src/os_main.c
+++ Core/Src/os_main.c
@@ -25,6 +25,10 @@
 */
 
 #include "ahura.h"
+#include "usbd_hid.h"
+#include "usb_device.h"
+
+#define HID_BUF_SIZE    4
 
 /*
  * ***********************************************************************************************************
@@ -41,9 +45,29 @@
  */
 void os_main(void)
 {
+    const int8_t x = 50;
+    const int8_t y = 50;
+    uint8_t hid_buffer[][HID_BUF_SIZE] = {
+        { 0, 0, y, 0 },
+        { 0, 0, -y, 0 },
+        { 0, x, 0, 0 },
+        { 0, -x, 0, 0 },
+        { 0, 0, -y, 0 },
+        { 0, 0, y, 0 },
+        { 0, -x, 0, 0 },
+        { 0, x, 0, 0 }
+    };
+    uint8_t i = 0;
+
     while (1)
     {
-        /* TODO: replace with the application's own code. */
-        os_delay_ms(1000U);
+        os_delay_ms(500);
+        HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+
+        // format: [7..3]  [2..0]       [7..0]         [7..0]        [7..0]
+        //         Empty  Buttons   X-axis(signed) Y axis(signed) Wheel(signed)
+        USBD_HID_SendReport(&hUsbDeviceFS,
+                            &hid_buffer[++i % (sizeof(hid_buffer) / HID_BUF_SIZE)][0],
+                            HID_BUF_SIZE);
     }
 }
