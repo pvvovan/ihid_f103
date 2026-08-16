b57cbd255d536d24360e54a00d8bb3c912a909be
Volodymyr Petrovskyy <pvvovan@gmail.com>
2026-08-16 12:17:20 +0200

    Add System View Description for debug

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
-cd ${SCRIPT_DIR}
+cd "${SCRIPT_DIR}"
 cmake -S . --preset=Debug -D OS_ARCH_VARIANT=cortex_m3
 cmake --build --preset=Debug
