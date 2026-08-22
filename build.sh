fa06065010a32449ac01ede36956734fafcb8f7a
Volodymyr Petrovskyy <pvvovan@gmail.com>
2026-08-22 10:40:09 +0200

    Pass presets file to cmake build command

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
-cd "${SCRIPT_DIR}"
 cmake -S . --preset=Debug -D OS_ARCH_VARIANT=cortex_m3
-cmake --build --preset=Debug
+cmake --build --preset=Debug --presets-file="${SCRIPT_DIR}/CMakePresets.json"
